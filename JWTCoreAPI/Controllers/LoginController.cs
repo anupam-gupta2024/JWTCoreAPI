50984c1dd8c9755db5db9828d8f94b7f08499e70
anupam.gupta@allen.in <anupam.gupta@allen.in>
2024-07-18 17:41:58 +0530

    Add project files.

---
+++ JWTCoreAPI/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using JWTCoreAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWTCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        private Users AuthenticateUser(Users users)
+        {
+            Users _user = null;
+            if (users.Username == "admin" && users.Password == "123456")
+            {
+                _user = new Users { Username = "Anupam Gupta" };
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users users)
+        {
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                null,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AuthenticateUser(user);
+            if(user_ != null)
+            {
+                var token  = GenerateToken(user_);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+    }
+}
---
+++ JWTCoreAPI/Controllers/SchoolController.cs
@@ -0,0 +1,34 @@
+﻿using JWTCoreAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with Jwt!!";
+        }
+
+        [HttpGet]
+        [Route("Detail")]
+        public string Detail()
+        {
+            return "No Authentication required";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddUser(Users user)
+        {
+            return "User added with username " + user.Username;
+        }
+    }
+}
